bd0ea5916d1c1422027132931b38ab5774589964
Evan Cooper <evcooper@wayfair.com>
2019-07-26 10:34:05 -0400

    Add how to page and github link to the header

---
+++ src/templates/howTo-template.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../components/Layout";
+import HowTo from "../components/HowTo";
+
+const HowToTemplate = () => {
+  return (
+    <Layout title="How To Get Started" description={""} isFullBleed >
+      <HowTo />
+    </Layout>
+  );
+};
+
+export default HowToTemplate;
