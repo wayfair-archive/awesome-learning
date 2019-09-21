004ad12eba11c38cffc680aed79c4b65e397f585
Shea Belsky <sheabelsky@gmail.com>
2019-09-21 11:43:56 -0400

    Remove useEffect hook

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -1,24 +1,15 @@
-import React, { useRef, useEffect } from 'react';
+import React from 'react';
 import './Page.scss';
 
-const Page = ({ title, children }) => {
-  const pageRef = useRef();
-
-  useEffect(() => {
-    pageRef.current.scrollIntoView();
-    window.scrollBy(0, -90);
-  });
-
-  return (
-    <div ref={pageRef} className="Page">
-      <div className="Page-inner">
-        { title && <h1 className="Page-title">{title}</h1>}
-        <div className="Page-body">
-          {children}
-        </div>
+const Page = ({ title, children }) => (
+  <div className="Page">
+    <div className="Page-inner">
+      { title && <h1 className="Page-title">{title}</h1>}
+      <div className="Page-body">
+        {children}
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default Page;
