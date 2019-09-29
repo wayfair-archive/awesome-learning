2bbbba7afd8553131995b2bea6ea0625f1c0849e
Shea Belsky <sheabelsky@gmail.com>
2019-09-29 13:45:02 -0400

    Fix ESLint and Prettier, add pre-commit hooks (#62)
    * Fix ESLint and Prettier, add pre-commit hooks
    
    * New line for Husky file

--- gatsby/pagination/create-courses-pages.js
+++ gatsby/pagination/create-courses-pages.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const path = require('path');
 const siteConfig = require('../../config.js');
 
--- gatsby/pagination/create-tags-pages.js
+++ gatsby/pagination/create-tags-pages.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const _ = require('lodash');
 const path = require('path');
 const siteConfig = require('../../config.js');
