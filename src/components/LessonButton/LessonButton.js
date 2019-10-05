4b3232c4b88877da37698b6ca3d284ecd2468572
Stan Faas <s.faas@youngcapital.nl>
2019-10-05 10:00:03 +0200

    fix: fixed linter errors

--- src/components/LessonButton/LessonButton.js
+++ src/components/LessonButton/LessonButton.js
@@ -57,9 +57,6 @@ const LessonButton = props => (
 );
 
 PureLessonButton.propTypes = {
-  path: PropTypes.string,
-  onClick: PropTypes.string,
-  children: PropTypes.string,
   data: PropTypes.string,
   defaultTab: PropTypes.string,
   path: PropTypes.string.isRequired,
