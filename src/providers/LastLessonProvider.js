797984e16a7c6effec3025c92aac404a8a82b047
Shea Belsky <sheabelsky@gmail.com>
2019-10-07 10:00:18 -0400

    Adding build step to GitHub actions, fixing LocalStorage error (#126)
    * Adding build step to GitHub actions, fixing LocalStorage error
    
    * Fix formatting of yaml
    
    * More descriptive and accurate name
    
    * Added guard around nullable storageType
    
    * Better ordering of hooks

--- src/providers/LastLessonProvider.js
+++ src/providers/LastLessonProvider.js
@@ -6,6 +6,7 @@ export const LastLessonContext = React.createContext();
 export const useLastLessonContext = () => useContext(LastLessonContext);
 
 const LastLessonProvider = ({children}) => {
+  const localStorage = typeof window !== "undefined" ? window.localStorage : null;
   const [lastLessonVisited, setLastLessonVisited] = useStorage(
     localStorage,
     'lastLessonVisited',
