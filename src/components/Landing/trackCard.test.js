e455698b38c9e95ebab3d521dd756919f4ccef30
Janessa Tran <tranjanessa@gmail.com>
2019-10-06 19:31:29 -0500

    Change casing to match import and update filename

--- src/components/Landing/trackCard.test.js
+++ src/components/Landing/trackCard.test.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import TrackCard from './TrackCard';
-import { getIcon } from "../../utils";
+import {render} from '@testing-library/react';
+import TrackCard from './trackCard';
+import {getIcon} from '../../utils';
 
 const PROPS = {
   title: 'Track Card Title',
-  icon: getIcon("array"),
+  icon: getIcon('array'),
   subTitle: 'This is a track card',
   path: '/trackcard'
 };
 
 describe('Track Card', () => {
   it('Renders an icon, title, and subtitle that link to path', () => {
-    const { container, getByText } = render(
-      <TrackCard {...PROPS} />
-    );
+    const {container, getByText} = render(<TrackCard {...PROPS} />);
     const icon = container.querySelector('.TrackCard-icon');
 
     expect(icon).toBeTruthy();
-    expect(container.querySelector(".TrackCard").getAttribute("to")).toBe(PROPS.path)
+    expect(container.querySelector('.TrackCard').getAttribute('to')).toBe(
+      PROPS.path
+    );
     expect(getByText(PROPS.title)).toBeTruthy();
     expect(getByText(PROPS.subTitle)).toBeTruthy();
   });
