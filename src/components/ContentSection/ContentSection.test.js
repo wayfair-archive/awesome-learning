fd05c7cc784cda3faef11458d0c2878d8238a871
Laura Barluzzi <laura@getadblock.com>
2019-10-10 21:13:27 -0400

    Remove wrong import

--- src/components/ContentSection/ContentSection.test.js
+++ src/components/ContentSection/ContentSection.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {render, getByText} from '@testing-library/react';
+import {render} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import ContentSection from './ContentSection';
 
