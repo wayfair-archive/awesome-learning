8b76466a770ab6472c3170870be5820d03baf7c1
Samaresh Sridhar <ssridhar@wayfair.com>
2019-10-14 17:19:08 -0400

    - ESlint and SCSSlint

--- src/components/shared/Icon/Icon.stories.js
+++ src/components/shared/Icon/Icon.stories.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { withKnobs } from '@storybook/addon-knobs';
-import IconReadme from './README.md'
-import { getIcon } from '../../../utils';
+import {storiesOf} from '@storybook/react';
+import {withKnobs} from '@storybook/addon-knobs';
+import IconReadme from './README.md';
+import {getIcon} from '../../../utils';
 import Icon from './Icon';
-
 
 const containerStyle = {
   display: 'flex',
@@ -26,73 +25,75 @@ const blockStyle = {
 
 const stories = storiesOf('Shared.Icon', module);
 
-stories.addDecorator(withKnobs)
+stories.addDecorator(withKnobs);
 stories.addParameters({
   readme: {
     // Show readme at the addons panel
-    sidebar: IconReadme,
-  },
-})
+    sidebar: IconReadme
+  }
+});
 
-stories
-  .add('basic', () =>
+stories.add(
+  'basic',
+  () => (
     <div style={containerStyle}>
       <div style={blockStyle}>
         <p>Twitter</p>
-        <Icon icon={getIcon("twitter")}/>
+        <Icon icon={getIcon('twitter')} />
       </div>
       <div style={blockStyle}>
         <p>Github</p>
-        <Icon icon={getIcon("github")}/>
+        <Icon icon={getIcon('github')} />
       </div>
       <div style={blockStyle}>
         <p>Email</p>
-        <Icon icon={getIcon("email")}/>
+        <Icon icon={getIcon('email')} />
       </div>
       <div style={blockStyle}>
         <p>Awesome Learning</p>
-        <Icon icon={getIcon("logo")}/>
+        <Icon icon={getIcon('logo')} />
       </div>
       <div style={blockStyle}>
         <p>Menu</p>
-        <Icon icon={getIcon("menu")}/>
+        <Icon icon={getIcon('menu')} />
       </div>
       <div style={blockStyle}>
         <p>Functions</p>
-        <Icon icon={getIcon("functions")}/>
+        <Icon icon={getIcon('functions')} />
       </div>
       <div style={blockStyle}>
         <p>Array</p>
-        <Icon icon={getIcon("array")}/>
+        <Icon icon={getIcon('array')} />
       </div>
       <div style={blockStyle}>
         <p>Async</p>
-        <Icon icon={getIcon("async")}/>
+        <Icon icon={getIcon('async')} />
       </div>
       <div style={blockStyle}>
         <p>Data</p>
-        <Icon icon={getIcon("data")}/>
+        <Icon icon={getIcon('data')} />
       </div>
       <div style={blockStyle}>
         <p>Testing</p>
-        <Icon icon={getIcon("testing")}/>
+        <Icon icon={getIcon('testing')} />
       </div>
       <div style={blockStyle}>
         <p>Function</p>
-        <Icon icon={getIcon("function")}/>
+        <Icon icon={getIcon('function')} />
       </div>
       <div style={blockStyle}>
         <p>Hook</p>
-        <Icon icon={getIcon("hook")}/>
+        <Icon icon={getIcon('hook')} />
       </div>
       <div style={blockStyle}>
         <p>Left Chevron</p>
-        <Icon icon={getIcon("leftChevron")}/>
+        <Icon icon={getIcon('leftChevron')} />
       </div>
       <div style={blockStyle}>
         <p>Right Chevron</p>
-        <Icon icon={getIcon("rightChevron")}/>
+        <Icon icon={getIcon('rightChevron')} />
       </div>
-    </div>,
-    { info: { disable: true } }
-  )
+    </div>
+  ),
+  {info: {disable: true}}
+);
