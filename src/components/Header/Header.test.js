65320795c48730f2a9528b0c018f21f32ccfd698
Eshno <oliver-felix@hotmail.com>
2019-10-17 08:00:11 -0400

    added header title and menulabels test

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,30 +1,20 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { PureHeader, Header } from './Header';
+import { PureHeader } from './Header';
 
 describe('PureHeader', () => {
-  it('renders', () => {
-    const menu = [{ label: "testlabel1", path: "/testlabel1" }]
-    const data = { site: { siteMetadata: { title: 'Test Title', menu } } };
-    const { container } = render(<PureHeader data={data} />);
-    expect(container).not.toBeNull();
+  const menu = [{ label: "testlabel1", path: "/testpath1" }, { label: "testlabel2", path: "/testpath2" }];
+  const title = 'Test Title';
+  const expectedData = { site: { siteMetadata: { title, menu } } };
+  
+  it('renders title correctly', () => {
+    const { getByText } = render(<PureHeader data={expectedData} />);
+    expect(getByText(title));
   })
 
-  it('renders menu correctly', () => {
-    const menu = [{ label: "testlabel1", path: "/testpath1" }, { label: "testlabel2", path: "/testpath2" }];
-    const data = { site: { siteMetadata: { title: 'Test Title', menu } } };
-    const { container, queryAllByText } = render(<PureHeader data={data} />);
-    expect(container.querySelectorAll('.Header-link')).toHaveLength(3);
-    expect(container.querySelectorAll('link[to="/testpath1"]')).toHaveLength(2);
-    expect(container.querySelectorAll('link[to="/testpath2"]')).toHaveLength(2);
-    expect(queryAllByText(/testlabel1/gi)).toHaveLength(2);
-    expect(queryAllByText(/testlabel2/gi)).toHaveLength(2);
-  })
-});
-
-describe('Header', () => {
-  it('renders', () => {
-    const { container } = render(<Header />);
-    expect(container).not.toBeNull();
+  it('renders menu labels correctly', () => {
+    const { getAllByText } = render(<PureHeader data={expectedData} />);
+    expect(getAllByText('testlabel1')).toHaveLength(2);
+    expect(getAllByText('testlabel2')).toHaveLength(2);
   })
 });
