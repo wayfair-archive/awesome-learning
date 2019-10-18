ae69204de0c504e913f9b6b891c51de5c19b8130
Oliver Félix Berrido <oliver-felix@hotmail.com>
2019-10-17 20:09:52 -0400

    Added tests for Header and PureHeader Components (#141)
    * Added Tests for Header and PureHeader
    
    * Fixed linter issues
    
    * added header title and menulabels test

---
+++ src/components/Header/Header.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { PureHeader } from './Header';
+
+describe('PureHeader', () => {
+  const menu = [{ label: "testlabel1", path: "/testpath1" }, { label: "testlabel2", path: "/testpath2" }];
+  const title = 'Test Title';
+  const expectedData = { site: { siteMetadata: { title, menu } } };
+  
+  it('renders title correctly', () => {
+    const { getByText } = render(<PureHeader data={expectedData} />);
+    expect(getByText(title));
+  })
+
+  it('renders menu labels correctly', () => {
+    const { getAllByText } = render(<PureHeader data={expectedData} />);
+    expect(getAllByText('testlabel1')).toHaveLength(2);
+    expect(getAllByText('testlabel2')).toHaveLength(2);
+  })
+});
