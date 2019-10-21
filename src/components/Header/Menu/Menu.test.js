280411d7f4d5310bc4117b6d249759be62c4c620
Daniella Taylor <daniella.taylor92@gmail.com>
2019-10-21 21:49:48 +0100

    Simple Menu Tests added

---
+++ src/components/Header/Menu/Menu.test.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Menu from './Menu';
+
+const MenuData = [
+  {
+    label: 'Menu Item 1',
+    path: '/item1'
+  },
+  {
+    label: 'Menu Item 2',
+    path: '/item2'
+  }
+];
+
+/**
+ * @param {NodeList} nodeList
+ * @param {string} attributeName
+ * @param {string} key
+ * @description
+ */
+function validateElementAttributeValue(nodeList, attributeName, key) {
+  return Array.from(nodeList).some(
+    node => node.getAttribute(attributeName) === key
+  );
+}
+
+describe('Menu Component', () => {
+
+  test('Renders a Menu', () => {
+    const { container } = render(
+      <Menu menu={MenuData} />
+    );
+    expect(container.querySelector('.Menu').textContent).toBeTruthy();
+  });
+
+  test('Menu has Menu Item 1 Item', () => {
+    const { container } = render(
+      <Menu menu={MenuData} />
+    );
+    expect(container.querySelector('.Menu-item').textContent).toContain('Menu Item 1');
+  });
+
+  test('Menu contains a link to /item2', () => {
+    const { container } = render(
+      <Menu menu={MenuData} />
+    );
+    const links = [...container.querySelectorAll('link')];
+
+    expect(validateElementAttributeValue(links, 'to', '/item2')).toBe(true);
+  })
+
+});
