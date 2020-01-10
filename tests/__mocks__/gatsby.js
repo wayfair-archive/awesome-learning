cc3be7d8d2a4e8770d98633b422c25f606c50e8c
Sidhartha Chatterjee <me@sidharthachatterjee.com>
2020-01-10 20:28:49 +0530

    Fix gatsby mocks

--- tests/__mocks__/gatsby.js
+++ tests/__mocks__/gatsby.js
@@ -1,8 +1,26 @@
-const gatsby = jest.requireActual('gatsby');
-
+const React = require("react")
+const gatsby = jest.requireActual("gatsby")
 module.exports = {
   ...gatsby,
   graphql: jest.fn(),
-  Link: 'Link',
-  StaticQuery: 'StaticQuery'
-};
+  Link: jest.fn().mockImplementation(
+    // these props are invalid for an `a` tag
+    ({
+      activeClassName,
+      activeStyle,
+      getProps,
+      innerRef,
+      partiallyActive,
+      ref,
+      replace,
+      to,
+      ...rest
+    }) =>
+      React.createElement("a", {
+        ...rest,
+        href: to,
+      })
+  ),
+  StaticQuery: jest.fn(),
+  useStaticQuery: jest.fn(),
+}
