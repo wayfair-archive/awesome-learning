0879d6c0d5542b0edec50859bebe4e31ef545a48
Christina Marie Rahaim <crahaim@wayfair.com>
2020-04-22 14:27:14 -0400

    Adds the ClickableCard component, stories, and tests.

---
+++ src/components/ClickableCard/ClickableCard.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { getIcon } from '../../utils';
+import Icon from '../shared/Icon';
+import './ClickableCard.scss';
+
+const ClickableCard = ({ children, link=null, handleCardClick=null, iconName=null }) => {
+  // Since either a link or onClick function is required, show an error to the developer if neither are passed in.
+  if (!link && !handleCardClick) {
+    console.error('No click action passed to ClickableCard. Please add a valid `link` or `handleCardClick` prop.');
+  }
+
+  const cardProps = link ? {href: link} : {onClick: handleCardClick};
+  return (
+    <a className="ClickableCard" {...cardProps} >
+      {iconName &&
+        <Icon icon={getIcon('hook')} cssClasses="ClickableCard-icon" />
+      }
+      {children}
+    </a>
+  );
+};
+
+ClickableCard.propTypes = {
+  link: PropTypes.string,
+  handleCardClick: PropTypes.func,
+  iconName: PropTypes.string
+};
+
+export default ClickableCard;
