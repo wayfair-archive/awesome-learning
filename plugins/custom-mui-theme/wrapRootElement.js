e8bfccb14549076a2cd2a2d6c1e222ceadcde35f
Eric Jacobson <ejacobson@wayfair.com>
2020-10-28 09:30:16 -0400

    update eslint config to allow non-default exports

--- plugins/custom-mui-theme/wrapRootElement.js
+++ plugins/custom-mui-theme/wrapRootElement.js
@@ -2,4 +2,4 @@ import React from "react";
 import { ThemeProvider } from "@material-ui/core/styles";
 import theme from "./theme";
 
-export default ({ element }) => <ThemeProvider theme={theme}>{element}</ThemeProvider>;
+export const wrapRootElement = ({ element }) => <ThemeProvider theme={theme}>{element}</ThemeProvider>;
