fae9804827594b0300beb10114158f3c6177ad4b
Eric Jacobson <ejacobson@wayfair.com>
2020-12-03 09:59:28 -0500

    finish up courses page

--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import Box from '@material-ui/core/Box';
 import {makeStyles, useTheme} from '@material-ui/core/styles';
@@ -6,14 +6,9 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Pagination from '@material-ui/lab/Pagination';
 
+const COURSES_LINK = '/courses';
+
 const useStyles = makeStyles((theme) => ({
-  pageTitle: {
-    textDecoration: 'underline',
-    margin: theme.spacing(3, 0, 6),
-  },
-  courseContainer: {
-    margin: theme.spacing(13, 0 , 14),
-  },
   title: {
     margin: theme.spacing(2, 0, 5),
     textDecoration: 'underline',
@@ -21,6 +16,9 @@ const useStyles = makeStyles((theme) => ({
       color: theme.palette.primary.main,
       textDecoration: 'none',
     },
+  },
+  courseContainer: {
+    margin: theme.spacing(10, 0 , 14),
   },
   goLearnCTA: {
     width: '140px',
@@ -32,36 +30,56 @@ const useStyles = makeStyles((theme) => ({
 const Courses = ({ edges }) => {
   const classes = useStyles();
   const theme = useTheme();
+  const itemsPerPage = 4;
+  const [page, setPage] = useState(1);
+  const noOfPages = Math.ceil(edges.length / itemsPerPage);
+  const handleChange = (event, value) => {
+    setPage(value);
+    window.scrollTo(0,0);
+  };
+
   return (
     <Box m="auto" maxWidth={theme.breakpoints.values.lg}>
-      <Typography variant="h1" className={classes.pageTitle}>Courses</Typography>
-      {edges.map(edge => (
-        <Box key={edge.node.fields.slug} className={classes.courseContainer}>
-          <Typography
-            variant="h3"
-            component="a"
-            display="block"
-            color="textPrimary"
-            href={edge.node.fields.slug}
-            className={classes.title}
-          >
-            {edge.node.frontmatter.title}
-          </Typography>
-          <Typography
-            color="textSecondary"
-            variant="body1"
-            component="a"
-            display="block"
-            href={edge.node.fields.categorySlug}
-            className={classes.title}
-          >
-            {edge.node.frontmatter.category}
-          </Typography>
-          <Typography variant="body1">{edge.node.frontmatter.description}</Typography>
-          <Button variant="contained" color="secondary" href={edge.node.fields.slug} className={classes.goLearnCTA}>Go Learn</Button>
-        </Box>
-      ))}
-      <Pagination count={10} variant="outlined" shape="rounded" />
+      <Typography variant="h1" color="textPrimary" component="a" href={COURSES_LINK} className={classes.title}>Courses</Typography>
+      {edges
+        .slice((page - 1) * itemsPerPage, page * itemsPerPage)
+        .map(edge => (
+          <Box key={edge.node.fields.slug} className={classes.courseContainer}>
+            <Typography
+              variant="h3"
+              component="a"
+              display="block"
+              color="textPrimary"
+              href={edge.node.fields.slug}
+              className={classes.title}
+            >
+              {edge.node.frontmatter.title}
+            </Typography>
+            <Typography
+              color="textSecondary"
+              variant="body1"
+              component="a"
+              display="block"
+              href={edge.node.fields.categorySlug}
+              className={classes.title}
+            >
+              {edge.node.frontmatter.category}
+            </Typography>
+            <Typography variant="body1">{edge.node.frontmatter.description}</Typography>
+            <Button variant="contained" color="secondary" href={edge.node.fields.slug} className={classes.goLearnCTA}>Go Learn</Button>
+          </Box>
+        ))}
+      <Box display="flex" justifyContent="center">
+        <Pagination
+          count={noOfPages}
+          page={page}
+          onChange={handleChange}
+          defaultPage={1}
+          variant="outlined"
+          shape="rounded"
+          size="large"
+        />
+      </Box>
     </Box>
   );
 };
