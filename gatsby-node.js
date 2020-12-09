43290bef17444d51c29b7eca34a4e889a0d1c27e
Eric Jacobson <ejacobson@wayfair.com>
2020-12-09 12:03:39 -0500

    use double quotes in node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,18 +3,18 @@ exports.onCreateNode = require('./gatsby/on-create-node');
 
 exports.onCreateBabelConfig = ({actions}) => {
   actions.setBabelPlugin({
-    name: 'babel-plugin-transform-imports',
+    name: "babel-plugin-transform-imports",
     options: {
-      '@material-ui/core': {
-        transform: '@material-ui/core/esm/${member}',
+      "@material-ui/core": {
+        transform: "@material-ui/core/esm/${member}",
         preventFullImport: true,
       },
-      '@material-ui/icons': {
-        transform: '@material-ui/icons/esm/${member}',
+      "@material-ui/icons": {
+        transform: "@material-ui/icons/esm/${member}",
         preventFullImport: true,
       },
-      '@material-ui/lab': {
-        transform: '@material-ui/lab/esm/${member}',
+      "@material-ui/lab": {
+        transform: "@material-ui/lab/esm/${member}",
         preventFullImport: true,
       },
     },
