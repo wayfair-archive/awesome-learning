c43b456ef446d662f5c7114d610fa8c624ca06ed
Eric Jacobson <ejacobson@wayfair.com>
2020-12-09 12:09:01 -0500

    disable eslint for transforms

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,14 +6,17 @@ exports.onCreateBabelConfig = ({actions}) => {
     name: "babel-plugin-transform-imports",
     options: {
       "@material-ui/core": {
+        /* eslint-disable-next-line */
         transform: "@material-ui/core/esm/${member}",
         preventFullImport: true,
       },
       "@material-ui/icons": {
+        /* eslint-disable-next-line */
         transform: "@material-ui/icons/esm/${member}",
         preventFullImport: true,
       },
       "@material-ui/lab": {
+        /* eslint-disable-next-line */
         transform: "@material-ui/lab/esm/${member}",
         preventFullImport: true,
       },
